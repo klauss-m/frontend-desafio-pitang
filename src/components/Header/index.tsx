a8da692dbf8bf52c464dd9f81103b7fcfc03b464
Klauss Marques <klauss.marques@gmail.com>
2022-04-23 02:49:09 -0300

    feat: changes to basic header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,15 @@
-import { Header as MantineHeader } from '@mantine/core';
+import { Header as MantineHeader, Group, Text } from '@mantine/core';
 
 export function Header() {
-  return <MantineHeader height={100}>teste</MantineHeader>;
+  return (
+    <MantineHeader height={60}>
+      <Group
+        sx={{ height: '100%' }}
+        px={20}
+        position='apart'
+      >
+        <Text>Pitang Vacinação</Text>
+      </Group>
+    </MantineHeader>
+  );
 }
