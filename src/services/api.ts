48fa443eeb08a65ede7a66fce97c0e718a2a05fa
Klauss Marques <klauss.marques@gmail.com>
2022-04-24 21:05:16 -0300

    ci: netlify deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: 'https://backend-desafio-pitang.herokuapp.com/',
   headers: { 'Access-Control-Allow-Origin': '*' },
 });
 
